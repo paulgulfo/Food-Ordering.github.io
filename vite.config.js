d16736a7b46395cfbeca832fdf9f64ed27520dd8
gulfopaul3@gmail.com <gulfopaul3@gmail.com>
2026-07-06 18:18:42 +0800

    build: output vite directly to root for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-});
+  build: {
+    outDir: '.', // Dito nito ilalagay ang built files sa labas mismo
+    assetsDir: 'assets',
+    emptyOutDir: false, // Huwag buburahin ang source files mo sa root!
+  }
+})
